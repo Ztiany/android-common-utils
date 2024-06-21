4a2b9315bd45dd7eafdeb95a670e6e43ed75a13a
Alien <ztiany3@gmail.com>
2024-06-21 18:41:22 +0800

    optimize code

--- src/main/java/com/android/base/utils/android/views/WebViewEx.kt
+++ src/main/java/com/android/base/utils/android/views/WebViewEx.kt
@@ -4,7 +4,36 @@ package com.android.base.utils.android.views
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.view.View
+import android.view.ViewGroup
 import android.webkit.WebView
+import android.webkit.WebViewClient
+
+/** Destroy the WebView. But it's better to run a WebView in a separate process. */
+fun WebView.destroyAll() {
+    try {
+        webChromeClient = null
+        setWebViewClient(object : WebViewClient() {
+            @Deprecated("Deprecated in Java", ReplaceWith("true"))
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                return true
+            }
+        })
+        webChromeClient = null
+        onPause()
+        if (parent != null) {
+            (parent as ViewGroup).removeAllViews()
+        }
+        visibility = View.GONE
+        removeAllViews()
+        if (handler != null) {
+            handler.removeCallbacksAndMessages(null)
+        }
+        destroy()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
 
 fun WebView.captureBitmapFromWebView(): Bitmap {
     val snapShot = this.capturePicture()
