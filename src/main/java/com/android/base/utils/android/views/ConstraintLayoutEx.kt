b3291c0935e5e8750974ba54cb7fb3fc5f38629e
Alien <ztiany3@gmail.com>
2024-08-23 18:29:44 +0800

    optimize code.

--- src/main/java/com/android/base/utils/android/views/ConstraintLayoutEx.kt
+++ src/main/java/com/android/base/utils/android/views/ConstraintLayoutEx.kt
@@ -3,9 +3,9 @@ package com.android.base.utils.android.views
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 
-fun ConstraintLayout.newConstraintSet(block: ConstraintSet.() -> Unit) {
+fun ConstraintLayout.applyNewConstraintSet(block: ConstraintSet.() -> Unit) {
     ConstraintSet().apply {
-        clone(this@newConstraintSet)
+        clone(this@applyNewConstraintSet)
         block()
     }.applyTo(this)
 }
